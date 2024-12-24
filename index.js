73c22f6c137ef3f3ec02bf10938984abefef1332
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-24 10:59:04 +0600

    Service get api added.

--- index.js
+++ index.js
@@ -39,14 +39,29 @@ async function run() {
         // Collections here
         const serviceCollection = database.collection("services");
 
-
         // Create router for add services
-        app.post('/addService', async (req, res) => {
+        app.post("/addService", async (req, res) => {
             const service = req.body;
             const result = await serviceCollection.insertOne(service);
             res.send(result);
-        })
+        });
 
+        // Create router for get services
+        app.get("/services", async (req, res) => {
+            try {
+                // Get limit from URL query
+                const limit = parseInt(req.query.limit) || 0;
+
+                // Fetch services with the specified limit or all services
+                const cursor = serviceCollection.find({});
+                const services = await cursor.limit(limit).toArray();
+
+                res.send(services);
+            } catch (error) {
+                console.error("Error fetching services:", error);
+                res.status(500).send({ message: "Error fetching services" });
+            }
+        });
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
