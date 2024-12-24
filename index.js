1330dbf094397729fbd4f1f0dc7db7963c307d5a
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-24 11:45:09 +0600

    Single service get api created.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,7 +11,6 @@ app.use(cors());
 
 // Atlas connection
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8eefy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -60,6 +60,20 @@ async function run() {
             } catch (error) {
                 console.error("Error fetching services:", error);
                 res.status(500).send({ message: "Error fetching services" });
+            }
+        });
+
+        // Create router for get service by id
+        app.get("/service/:id", async (req, res) => {
+            try {
+                const id = req.params.id;
+                const service = await serviceCollection.findOne({
+                    _id: new ObjectId(id),
+                });
+                res.send(service);
+            } catch (error) {
+                console.error("Error fetching service:", error);
+                res.status(500).send({ message: "Error fetching service" });
             }
         });
     } finally {
